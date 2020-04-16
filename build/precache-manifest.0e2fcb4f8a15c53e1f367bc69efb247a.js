7fc7b114bb09fb915b2fdea0c08bd729cc1fca5f
melwinalm <melwinalm@gmail.com>
2020-04-16 22:10:56 +0530

    Adds build directory

---
+++ build/precache-manifest.0e2fcb4f8a15c53e1f367bc69efb247a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d32934e4ba23fa425db8d46683cf572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71e0538d11f0864a2dd",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e8a3d3f4e5d2bc9b0adf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b71e0538d11f0864a2dd",
+    "url": "/static/js/2.c6b4b479.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c6b4b479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a3d3f4e5d2bc9b0adf",
+    "url": "/static/js/main.f1ca91b4.chunk.js"
+  },
+  {
+    "revision": "0863b16f5085b67b6be8",
+    "url": "/static/js/runtime-main.ec679fab.js"
+  }
+]);
